41c37aa6e77333997e3030124e1e45ef61bcb258
sam <lipschutz@nscl.msu.edu>
2013-03-20 11:52:41 -0400

    Re did TOF_res.C.  Now reads tree explictly instead of taking projections.  requires that both top and bottom PMTs to be above energy threshold

--- TOF_res.C
+++ TOF_res.C
@@ -7,6 +7,122 @@ TGraphErrors* TOF_res(){
   Double_t startGamma=-15.6;
   Double_t endGamma=-14.6;
 
+  const Int_t numProjections=40;
+
+  Double_t range=400;
+  Double_t startPoint=0;
+  Double_t step =(range-startPoint)/numProjections;
+  TH1D * projections[numProjections];
+  TH1D * projectionsOrig[numProjections];
+
+  Double_t cutE=0;
+
+
+
+  LendaEvent * Event = new LendaEvent();
+  
+  flt->SetBranchAddress("Event",&Event);
+  
+  Int_t numEntries = flt->GetEntries();
+  
+  std::stringstream s;
+  
+  for (int j=0;j<numProjections;j++){
+
+    
+    cutE = startPoint + j*step;
+    cout<<"On "<<cutE<<endl;
+    s<<"E_"<<startPoint+j*step;
+
+    projections[j]=new TH1D(s.str().c_str(),"TOF",1000,-20,20);
+    s.str("");
+    s<<"OrigE_"<<cutE;
+    projectionsOrig[j]=new TH1D(s.str().c_str(),"TOF",1000,-20,20);
+    s.str("");
+    for (int I=0;I<numEntries;I++){
+      
+      flt->GetEntry(I);
+      
+      if ( Event->channels[0] ==2 && Event->channels[2]==8 && TMath::Abs(Event->CorGOE)<0.5 && (Event->PulseShape)<1.12){
+	
+	if (Event->energiesCor[0]>= cutE && Event->energiesCor[1]>=cutE){
+	  
+	  projections[j]->Fill(Event->TOFW[1]);
+	  projectionsOrig[j]->Fill(Event->ShiftTOF);
+	}
+	
+      }
+      
+    }
+    
+  }
+
+
+  Double_t x[numProjections];
+  Double_t y[numProjections];
+  Double_t ex[numProjections];
+  Double_t ey[numProjections];
+
+  Double_t x1[numProjections];
+  Double_t y1[numProjections];
+  Double_t ex1[numProjections];
+  Double_t ey1[numProjections];
+
+  for (int i=0;i<numProjections;i++){
+    x[i]=startPoint + i*step;
+    x1[i]=startPoint + i*step;
+    ex[i]=0;
+    ex1[i]=0;
+    y[i]=y1[i]=ey[i]=ey1[i]=0;
+  }
+
+  TF1 *f1 = new TF1("f1","gaus",startGamma,endGamma);
+  
+  TFitResultPtr result;
+  for(int i=0;i<numProjections;i++){
+    result = projections[i]->Fit("f1","QSNR");
+    Int_t status=result;
+    if (status==0){
+      y[i]=result->Value(2);
+      ey[i]=result->UpperError(2);
+    } else {
+      y[i]=0;
+      ey[i]=0;
+    }
+
+    result = projectionsOrig[i]->Fit("f1","QSNR");
+    status=result;
+    if (status==0){
+      y1[i]=result->Value(2);
+      ey1[i]=result->UpperError(2);
+    } else {
+      y1[i]=0;
+      ey1[i]=0;
+    }
+
+  }
+
+  TGraphErrors *gr =new TGraphErrors(numProjections,x,y,ex,ey);
+  TGraphErrors *grOrig =new TGraphErrors(numProjections,x1,y1,ex1,ey1);
+  
+  grOrig->SetLineColor(2);
+  
+  TCanvas *c = new TCanvas("c22");
+
+  c->cd(1);
+  gr->Draw("AL*");
+  grOrig->Draw("same");
+  return gr;
+  
+}
+/*
+
+
+
+
+
+
+
   if (gROOT->GetListOfSpecials()->IsEmpty()){
     cout<<"NO TCutG found \nMake a PulseShape cut first"<<endl;
     return;
@@ -49,32 +165,13 @@ TGraphErrors* TOF_res(){
   }
 
 
-  TF1 *f1 = new TF1("f1","gaus",startGamma,endGamma);
-  
 
-  TFitResultPtr result;
-  for(int i=0;i<numProjections;i++){
-    result = projections[i]->Fit("f1","QSNR");
-    Int_t status=result;
-    if (status==0){
-      y[i]=result->Value(2);
-      ey[i]=result->UpperError(2);
-    } else {
-      y[i]=0;
-      ey[i]=0;
-
-    }
     
     //    cout<<x[i]<<" "<<ex[i]<<endl;
 
   }
 
-  TGraphErrors *gr =new TGraphErrors(numProjections,x,y,ex,ey);
-  
-  TCanvas *c = new TCanvas("c22");
 
-  c->cd(1);
-  gr->Draw("AL*");
 
 
 
@@ -82,3 +179,4 @@ TGraphErrors* TOF_res(){
   return gr;
 
 }
+*/
