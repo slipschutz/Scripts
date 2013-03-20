61e4d4a4be821673fcb768f5e97c5b3733d48a22
Samuel Lipschutz <lipschut@nscl.msu.edu>
2013-03-19 22:21:40 -0400

    commit

--- TOF_res.C
+++ TOF_res.C
@@ -1,11 +1,11 @@
 
 
 
-TGraphErrors* TOF_walk(){
+TGraphErrors* TOF_res(){
 
 
   Double_t startGamma=-15.6;
-  Double_t endGamma=-14.8;
+  Double_t endGamma=-14.6;
 
   if (gROOT->GetListOfSpecials()->IsEmpty()){
     cout<<"NO TCutG found \nMake a PulseShape cut first"<<endl;
@@ -15,11 +15,11 @@ TGraphErrors* TOF_walk(){
   TH2F * E_vs_TOF =new TH2F("E_vs_TOF","E_vs_TOF",1000,-20,20,1000,0,2000);
 
 
-  flt->Project("E_vs_TOF","energiesCor[1]:TOF",
+  flt->Project("E_vs_TOF","energiesCor[0]:TOFW[1]",
 	       "CUTG && channels[0]==2&&abs(ShiftTOF)<20&&abs(CorGOE)<0.5&&channels[2]==8");
 
   Int_t channel=0;
-  const Int_t numProjections=40;
+  const Int_t numProjections=100;
 
   Double_t range=600;
   Double_t startPoint=0;
@@ -36,8 +36,8 @@ TGraphErrors* TOF_walk(){
   std::stringstream s;
   for (int i=0;i<numProjections;i++){
     Int_t bin1 = E_vs_TOF->GetYaxis()->FindBin(startPoint +i*step);
-    Int_t bin2 = E_vs_TOF->GetYaxis()->FindBin(startPoint+ (i+1)*step);
-    ///     Int_t bin2 = channel[j]->GetYaxis()->FindBin(600);
+    //    Int_t bin2 = E_vs_TOF->GetYaxis()->FindBin(startPoint+ (i+1)*step);
+    Int_t bin2 = E_vs_TOF->GetYaxis()->FindBin(1600);
     x[i]=startPoint+i*step;
     ex[i]=0;
     
@@ -57,15 +57,15 @@ TGraphErrors* TOF_walk(){
     result = projections[i]->Fit("f1","QSNR");
     Int_t status=result;
     if (status==0){
-      y[i]=result->Value(1);
-      ey[i]=result->UpperError(1);
+      y[i]=result->Value(2);
+      ey[i]=result->UpperError(2);
     } else {
       y[i]=0;
       ey[i]=0;
 
     }
     
-    cout<<y[i]<<" "<<ey[i]<<endl;
+    //    cout<<x[i]<<" "<<ex[i]<<endl;
 
   }
 
